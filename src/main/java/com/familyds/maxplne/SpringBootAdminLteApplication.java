e504a2b17583987fee8490767b2fbb8222a964e0
xinyuan yuan <maxplne2012@gmail.com>
2019-05-30 14:10:50 +0000

    first commit

---
+++ src/main/java/com/familyds/maxplne/SpringBootAdminLteApplication.java
@@ -0,0 +1,13 @@
+package com.familyds.maxplne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminLteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminLteApplication.class, args);
+	}
+
+}
